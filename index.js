89a97e65041c3d8620d89a51bd4fa9b81a841522
Manas Bhatia <manasbhatiaprsnl@gmail.com>
2024-12-28 23:28:28 +0530

    github api added

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ require("dotenv").config()
 const app = express() // Now we have the app made
 const port = process.env.PORT // We can use any of the many virtual ports on a computer ie backend ko listen kahan krna hai
 
+const response = fetch("https://api.github.com/users/manasbhatia5128");
+let retrivedData = null;
+response.then((resp) => {
+  if(!resp.ok){
+    throw new Error("Error while retriving data")
+  }
+  else{
+    return JSON.parse(resp);
+  }
+})
+.then((data) => {
+  retrivedData = data;
+})
+.catch((e) => console.log(e));
+
 // app.get({konse route pr listen kr rhe ho}, {krna kya hai tb, pass as a callback})
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -15,6 +30,10 @@ app.get('/sample', (req, res)=>{
 
 app.get('/login', (req, res) => {
     res.send("<h1>Sample data</h1>"); // You can send any type of data
+})
+
+app.get('github', (req, res) => {
+  res.send(retrivedData);
 })
 
 app.listen(port, () => { // process.env use krna pdta hai
